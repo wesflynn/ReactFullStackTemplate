39b72955d9b672e85ffb2269614ff094ce648963
wesflynn <wesley.flynn@mygeorgian.ca>
2018-06-23 21:03:50 -0400

    init

---
+++ src/client/app.js
@@ -0,0 +1,20 @@
+import React, { Component, } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
